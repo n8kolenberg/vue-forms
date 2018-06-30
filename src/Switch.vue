6112bf39e0cf279a3d940a0b20ffd95f4f784929
Nathaniel Kolenberg <nathanielkolenberg@Nathaniels-MacBook-Pro.local>
2018-06-30 19:06:58 +0400

    Create simple vue form app that showcases some of the cool things you can do with vue and forms

---
+++ src/Switch.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div
+                id="on"
+                @click="switched(true)"
+                :class="{active: value}">On</div>
+        <div
+                id="off"
+                @click="switched(false)"
+                :class="{active: !value}">Off</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['value'],
+        methods: {
+            switched(isOn) {
+                //Emitted event has to be called input because that's what v-model is waiting for in App.vue on app-switch
+                this.$emit("input", isOn)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #on, #off {
+        width: 40px;
+        height: 20px;
+        background-color: lightgray;
+        padding: 2px;
+        display: inline-block;
+        margin: 10px -2px;
+        box-sizing: content-box;
+        cursor: pointer;
+        text-align: center;
+    }
+
+    #on:hover, #on.active {
+        background-color: lightgreen;
+    }
+
+    #off:hover, #off.active {
+        background-color: lightcoral;
+    }
+</style>
